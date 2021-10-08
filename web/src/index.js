dafa4766c3bc8029e5df0eb14de5814190861436
Alexandra Lillo <stereodreaming@gmail.com>
2021-10-08 10:50:16 +0200

    all files updated

--- web/src/index.js
+++ web/src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
+import App from "./components/App";
+import "./stylesheets/index.scss";
 
 ReactDOM.render(
   <HashRouter>
     <App />
   </HashRouter>,
-  document.getElementById('root')
+  document.querySelector("#root")
 );
