bf8448ef03fc0895cc04dec1ff33644e31659498
CriSFV <cristina.f.valverde@gmail.com>
2021-10-13 18:51:21 +0200

    add static server

--- src/index.js
+++ src/index.js
@@ -14,5 +14,18 @@ server.listen(serverPort, () => {
 });
 
 server.get('/movies', (req, res) => {
+  const genderFilterParam = req.query.gender;
+  // console.log(genderFilterParam);
+  // console.log(movies);
+  // const filteredMovies = movies.filter((movie) => {
+  //   movie.gender === genderFilterParam.gender;
+  // });
+  console.log(genderFilterParam);
   res.json(movies);
 });
+
+const staticServerPathWeb = './public'; // En esta carpeta ponemos los ficheros estáticos
+server.use(express.static(staticServerPathWeb));
+
+const staticServerPathWeb2 = './src/public-movies-images'; // En esta carpeta ponemos los ficheros estáticos
+server.use(express.static(staticServerPathWeb2));
--- web/src/services/api-movies.js
+++ web/src/services/api-movies.js
@@ -1,10 +1,11 @@
 // login
-
-const getMoviesFromApi = () => {
+//Cris: añado props que es el objeto  sta mandando el parametro desde App.js (params))
+const getMoviesFromApi = (params) => {
   console.log('Se están pidiendo las películas de la app');
+  console.log(params);
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
   // '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/empty.json'
-  return fetch('//localhost:4000/movies', {
+  return fetch(`//localhost:4000/movies?gender=${params.gender}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
